598c62d83d65c18eef1daee80be0ef9791a4b48e
Trilok Acharya <tacharya@deerwalk.com>
2014-03-02 21:18:27 -0500

    - Adding a bunch of aggregations around the sender's stats

--- src/main/scala/JSONMailParser.scala
+++ src/main/scala/JSONMailParser.scala
@@ -1,3 +1,4 @@
+import cascading.operation.Buffer
 import com.twitter.scalding._
 import org.apache.hadoop.util.ToolRunner
 import org.apache.hadoop.conf.Configuration
@@ -17,37 +18,33 @@ class JSONMailParser(args:Args) extends Job(args)
 {
   implicit val formats = org.json4s.DefaultFormats // required for extracting object out of JSON
 
-  val fieldSchema = ('success,'from,'to,'subject,'date,'attachments,'labels,'body,'comment)
-  val finalSchema = ('from,'to,'subject,'date,'attachments,'labels,'comment)
+  val fieldSchema = ('success,'from,'to,'subject,'date,'attachments,'labels,'body)
+  val finalSchema = ('from,'to,'subject,'date,'attachments,'labels,'comment,'body)
 
-  TextLine(args("input"))
+  val parsedPipe=TextLine(args("input"))
     .addTrap(Csv("resources/errors"))
     .map('line->fieldSchema)
   {
     line:String => {
-      //try{
         val parsed=JsonMethods.parse(line).extract[mailJSON] // parse JSON and then "unmarshall" into mailJSON case class
         // See coments for mailJSON case class regarding why these fields aren't wrapped in Option[T]
-        (1,scrubAddresses(parsed.from),parsed.to,parsed.subject.trim,parsed.parsedDate.toString("yyyy:MM:dd"),parsed.attachments,parsed.labels.trim,parsed.body,"")
-/*      }
-      catch { // in case of any exceptions, we return a tuple of empty strings. The arity of the tuple in try and catch need to match
-        case e:Exception => (0,"","","","","","","") // 0 for the value of "success" means it failed
-      }*/
+      (1,scrubAddresses(parsed.from),parsed.to,parsed.subject.trim,parsed.parsedDate,parsed.attachments,parsed.labels.trim,parsed.body)
     }
   }
-    //.filter('success){ success: Int => success ==1 } // only take successfully parsed tuples
+    //.filter('success){ success: Int => success ==1 } // only take successfully parsed tuples. Currently not used
     .filter('labels){ labels: String => !(labels.split(",").contains("Chat"))} // ignore Chat messages
-    .project(fieldSchema)
     .flatMap('to-> 'to) { to:String=>to.split(",").map{s=>scrubAddresses(s)} } // each recipient gets their own tuple
-/*    .groupBy('to,'date) {
-          grp => grp.size
-                    //.average(grp.)
-
-     }*/
-    //.project('subject,'to)
     .project(finalSchema)
-    .write(Csv(args("output")))
 
+  val personStat = parsedPipe.map('date->'isWeekend){d:DateTime => d.getDayOfWeek > 5}
+                  .map('body -> 'mailLength) {b:String => b.length}
+                  .map('subject -> 'subjLength) {s:String => s.length}
+                  // group by sender and whether it was sent on a weekend. Then count the number of messages in that group
+                  // and get the average length of messages in each group along with average subject length in each group
+                  .groupBy('from,'isWeekend)(_.size('count).average('mailLength).average('subjLength))
+                  .groupAll(_.sortBy('count)) // order by message count
+
+    personStat.write(Csv(args("output"),writeHeader=true))
   /**
    * Function to sanitize email addresses
    * @param addr
@@ -56,7 +53,6 @@ class JSONMailParser(args:Args) extends Job(args)
   def scrubAddresses(addr:String)={
     val toRemove:List[String]=List("\t","\n","\r","\"","'")
     toRemove.foldLeft(addr)((a,rem)=>a.replace(rem,"")).trim
-    //addr.replace("\"","").replace("'","").trim
   }
 
 }
